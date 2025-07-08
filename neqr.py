01c93bbdfee41d10180f61a19d61f278fe9482c8
RYZEXY <sunfred88@gmail.com>
2025-07-08 18:16:35 -0400

    proof of concept

--- neqr.py
+++ neqr.py
@@ -9,33 +9,36 @@ import numpy as np
 from qiskit.circuit.library.basis_change import QFT
 
 
-def neqr(intensity,x,y,image):
-
+def neqr(intensity, x, y, image):
     intensity_bits = len(intensity)
+    num_x_bits = len(x)
+    num_y_bits = len(y)
     position = list(x) + list(y)
-    position_bits = len(position)
-    qc = QuantumCircuit(intensity,position, name = "NEQR")
+    position_bits = num_x_bits + num_y_bits
+    qc = QuantumCircuit(intensity, position, name="NEQR")
 
     flat_image = image.flatten()
 
-    for pos in range(position_bits ** 2):
-        intensity = int(flat_image[pos])
-        bin_intensity = format(intensity, f'0{intensity_bits}b')[::-1]  # 8 qubit binary
+    for pos in range(2 ** position_bits):
+        pixel_intensity = int(flat_image[pos])
+        bin_intensity = format(pixel_intensity, f'0{intensity_bits}b')[::-1] 
+        control_bits = format(pos, f'0{position_bits}b')[::-1] 
 
-        # Control based on position qubits
-        control_bits = format(pos, '02b')  # binary position
         controls = []
         for j, cb in enumerate(control_bits):
             qubit = intensity_bits + j
             if cb == '0':
                 qc.x(qubit)
             controls.append(qubit)
-        
-        for i, bit in enumerate(bin_intensity):
-            if bit =='1':
-                qc.mcx(controls, i) 
 
+        for i, bit in enumerate(bin_intensity):
+            if bit == '1':
+                qc.mcx(controls, i)
+
+        # Uncompute X gates
         for j, cb in enumerate(control_bits):
-                    if cb == '0':
-                        qc.x(intensity_bits + j)  # Undo X
+            if cb == '0':
+                qc.x(intensity_bits + j)
+
     return qc.to_gate()
+
